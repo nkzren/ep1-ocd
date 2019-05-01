36385cac33d50ebf1b73b4d62c37aa1bf97869a9
nkzren <renan.nakazawa0@gmail.com>
2019-05-01 13:04:55 -0300

    funcoes basicas

--- src/Booth.java
+++ src/Booth.java
@@ -8,40 +8,110 @@
  */
 public class Booth {
 	public int[] binario1;
+	public int[] binario1Negativo;
 	public int[] binario2;
 	public int[] produtoBinario;
-
-	public int toDecimal(int[] binario) {
-		return 0;
-	}
-
-	public int[] toBinario(int inteiro) {
-        int[] binario = new int[16];
-        int index = 15;
-        int num = inteiro;
-        
-        if (inteiro < 0) {
-        	num = (int) Math.pow(2, binario.length) + inteiro;
-        }
-
-        while (num != 0) {
-            binario[index--] = num % 2;
-            num /= 2;
-        }
-
-        return binario;
-	}
-
-	public void rightShift(int[] binario) {
-        for (int i = binario.length; i >= 1; i--) {
-        	binario[i] = binario[i - 1];                	
-        }
-	}
 	
-	public void imprime(int[] binario, char tipo) {
+	
+	public void multiplicar(int n1, int n2) {
 		
 	}
+
+	/**
+	 * @description Soma dois números binários e armazena o resultado em bin1
+	 * @param bin1 array com o primeiro binário. O resultado da soma é armazenado
+	 *             aqui
+	 * @param bin2 array com o segundo binário.
+	 * @returns void
+	 * @throws IllegalArgumentException se os arrays representando os binários não
+	 *                                  tiverem o mesmo tamanho (Erro na conversão)
+	 */
+	public void add(int[] bin1, int[] bin2) throws IllegalArgumentException {
+		if (bin1.length != bin2.length) {
+			throw new IllegalArgumentException("Erro na conversão dos binários");
+		}
+
+		int aux = 0;
+		for (int i = bin1.length; i >= 0; i--) {
+			int temp = bin1[i] + bin2[i] + aux;
+			bin1[i] = temp % 2;
+			aux = temp / 2;
+		}
+
+	}
+
+	/**
+	 * @description Converte um binário em um decimal (foi utilizado long porque não
+	 *              cabia no int para a quantidade de bits utilizada)
+	 * @param binario Número binário em forma de array de int
+	 * @returns long
+	 */
+	public long toDecimal(int[] binario) {
+		long decimal = 0;
+		int aux = 1;
+		for (int i = binario.length - 1; i >= 0; i--) {
+			decimal += binario[i] * aux;
+			aux *= 2;
+		}
+		if (decimal > 1073741824) {
+			decimal = -(4294967296l - decimal);
+		}
+		return decimal;
+	}
+
+	/**
+	 * @description Converte um número inteiro para um array de int na forma
+	 *              binária. Números negativos são convertidos em complemento de 2
+	 * @param inteiro Número inteiro
+	 * @returns int[]
+	 */
+	public int[] toBinario(int inteiro) {
+		int[] binario = new int[16];
+		int index = 15;
+		int num = inteiro;
+
+		// Converte para complemento de 2
+		if (inteiro < 0) {
+			num = (int) Math.pow(2, binario.length) + inteiro;
+		}
+
+		while (num != 0) {
+			binario[index--] = num % 2;
+			num /= 2;
+		}
+
+		return binario;
+	}
+
+	/**
+	 * @description Desloca o binário para a direita
+	 * @param binario Número binário em forma de array de int
+	 */
+	public void rightShift(int[] binario) {
+		for (int i = binario.length - 1; i >= 1; i--) {
+			binario[i] = binario[i - 1];
+		}
+	}
+
+	/**
+	 * @description Imprime um binário no console, acompanhado do caracter do segundo parâmetro
+	 * @param binario Número binário em forma de array de int
+	 * @param tipo Caracter para demonstrar o tipo de binário
+	 */
+	public void imprimeBinario(int[] binario, char tipo) {
+
+	}
 	
-	
+	/**
+	 * @description Função que valida um número dado
+	 * @returns boolean
+	 */
+	public boolean isNumeroInvalido(int[] num) {
+		if (null == num || toDecimal(num) < -32767 || toDecimal(num) > 32767) {
+			System.out.println("Número inválido inserido. Digite novamente");
+			return true;
+		}
+		return false;
+	}
 
 }
--- src/EP1OCD.java
+++ src/EP1OCD.java
@@ -1,45 +1,52 @@
 import java.util.Scanner;
 
 /**
- * @author 	Renan Nakazawa
- * @nUsp	10723836
- * @description Classe principal do EP
+ * @author Renan Nakazawa
+ * @nUsp 10723836
+ * @description Classe que contém o método main do EP
  */
 public class EP1OCD {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Iniciando multiplicador binário");
-		System.out.println("Digite dois números inteiros para iniciar. (Módulo máximo: 16384)");
-		Scanner s;
-		s = new Scanner(System.in);
-		Integer num1 = null; 
-		Integer num2 = null;
+		System.out.println("Iniciando multiplicador binário\n");
+		System.out.println("Instruções: O usuário entra com 2 números binários a serem multiplicados\n"
+				+ "Os números então são exibidos na forma inteira e seu resultado é apresentado\n"
+				+ "nas formas inteira e binária. Números negativos devem ser em complemento de 2\n");
+		System.out.println("Digite dois números inteiros para iniciar. (Máximo: 16 bits)");
+
+		Booth booth = new Booth();
+
+		Scanner s = new Scanner(System.in);
+		int[] num1 = new int[33];
+		int[] num2 = new int[33];
+		String input1;
+		String input2;
 		try {
-			do {
-				System.out.print("Primeiro Número: ");
-				if (s.hasNextInt()) {
-					num1 = s.nextInt();					
-				}
-				System.out.print("Segundo Número: ");
-				if (s.hasNextInt()) {
-					num2 = s.nextInt();					
-				}
-			} while (!validarNumero(num1) || !validarNumero(num2));
+			System.out.print("Primeiro Número: ");
+			while (!s.hasNextInt()) {
+				System.out.println("Entrada Inválida! Digite novamente");
+				s.next();
+			}
+			input1 = s.next();
+
+			System.out.print("Segundo Número: ");
+			while (!s.hasNextInt()) {
+				System.out.println("Entrada Inválida! Digite novamente");
+				s.next();
+			}
+			input2 = s.next();
+
+			for (int i = num1.length - input1.length(); i < num1.length; i++) {
+				num1[i] = input1.charAt(i - num1.length + input1.length()) - 48;
+			}
+			for (int i = num2.length - input2.length(); i < num2.length; i++) {
+				num2[i] = input2.charAt(i - num2.length + input2.length()) - 48;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		Booth booth = new Booth();
-		
+
 		s.close();
 	}
-	
-	public static boolean validarNumero(Integer num) {
-		if (null == num || num < -16384 || num > 16384) {
-			System.out.println("Número inválido inserido. Digite novamente");
-			return false;
-		}
-		return true;
-	}
-	
+
 }
